0c132fb5f6494c521bb8899350cbf9435287ddc6
Dmitry.Gridnev <dmitry.gridnev@testit.software>
2023-01-20 14:00:46 +0100

    Fixed bug with link convert

--- testgear-importer-allure/src/converter.py
+++ testgear-importer-allure/src/converter.py
@@ -74,13 +74,15 @@ class Converter:
     def link_to_link_post_model(
             url: str,
             title: str,
-            url_type: str,
+            url_type,
             description: str):
         if url_type:
+            if type(url_type) is str:
+                url_type = LinkType(value=url_type)
             return LinkPostModel(
                 url,
                 title=title,
-                type=LinkType(value=url_type),
+                type=url_type,
                 description=description
             )
         else:
@@ -94,13 +96,15 @@ class Converter:
     def link_to_link_put_model(
             url: str,
             title: str,
-            url_type: str,
+            url_type,
             description: str):
         if url_type:
+            if type(url_type) is str:
+                url_type = LinkType(value=url_type)
             return LinkPutModel(
                 url,
                 title=title,
-                type=LinkType(value=url_type),
+                type=url_type,
                 description=description
             )
         else:
